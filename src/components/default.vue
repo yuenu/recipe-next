07aa777ee921cbc422da29b7ae779650fd84603c
MSI\yuenu <yuenuni@hotmail.com>
2021-02-02 15:10:47 +0800

    initial

---
+++ src/components/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="default">
+    <p>This is default Page</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+})
+</script>
+
+<style lang='scss' scoped>
+
+</style>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <p>This is Header Page</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+})
+</script>
+
+<style lang='scss' scoped>
+
+</style>
