be5fc78ce5aacb80c54bbbe4753b67645bff21fa
yuenu <yuenuni@hotmail.com>
2021-04-05 21:31:55 +0800

    feat: google map done

--- src/components/recipe/Home/GoogleMap.vue
+++ src/components/recipe/Home/GoogleMap.vue
@@ -1,44 +1,62 @@
 <template>
-  <div class="googleMap" style="height: 20vh;">
-    <h2>lat: {{ currPos.lat }}</h2>
-    <h2>lng: {{ currPos.lng }}</h2>
+  <div class="googleMap">
+    <div
+      class="mapDiv"
+      ref="mapDiv"
+    ></div>
   </div>
-  <div ref="mapDiv" style="width:100%; height:80vh;"></div>
+
 </template>
 
 <script lang="ts">
-/* eslint-disable no-undef */
-import { defineComponent, computed, onMounted, ref } from 'vue'
-import { useGeolocation } from '@/plugin/useGeoloactionl'
+import { defineComponent, onMounted, ref } from 'vue'
 import { Loader } from '@googlemaps/js-api-loader'
 
 const googleMapsApiKey = 'AIzaSyCs74COIjvF20Nf-Su3NIFlmCSQ0iAy_g4'
+const TaichungCityGovernment = { lat: 24.161796, lng: 120.646765 }
 
 export default defineComponent({
   name: 'GoogleMap',
 
   setup () {
     const mapDiv = ref<HTMLElement>()
-    const { latitude, longitude } = useGeolocation().value
 
-    const currPos = computed(() => ({
-      lat: latitude,
-      lng: longitude
-    }))
-    // const testgoogle = new google
-    const loader = new Loader({ apiKey: googleMapsApiKey })
+    const loader = new Loader({ apiKey: googleMapsApiKey, language: 'zh_TW' })
+
+    function initMap () {
+      if (!mapDiv.value) { return }
+
+      const map = ref<google.maps.Map>()
+      const marker = ref<google.maps.Marker>()
+      const geocoder = new google.maps.Geocoder()
+
+      map.value = new google.maps.Map(mapDiv.value, {
+        center: TaichungCityGovernment,
+        zoom: 16
+      })
+
+      geocoder.geocode({ address: 'Taichung City Government' }, (results, status) => {
+        if (status === 'OK' && results) {
+          if (!map.value) return
+          map.value.setCenter(results[0].geometry.location)
+          marker.value = new google.maps.Marker({
+            map: map.value,
+            position: results[0].geometry.location
+          })
+        } else {
+          console.log(
+            'Geocode was not successful for the following reason: ' + status
+          )
+        }
+      })
+    }
 
     onMounted(async () => {
       await loader.load()
-      // const map = new google.maps.Map(mapDiv.value, {
-      //   center: { lat: -34.397, lng: 150.644 },
-      //   zoom: 8
-      // })
+      initMap()
     })
-    // const currPos = { lat: 40.689247, lng: -74.044502 }
 
     return {
-      currPos,
       mapDiv
     }
   }
@@ -46,5 +64,12 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-
+.googleMap {
+  width:100%;
+  margin: 0 0 5rem 0;
+}
+.mapDiv {
+  width:100%;
+  height:60vh;
+}
 </style>
--- src/plugin/useGeoloactionl.ts
+++ src/plugin/useGeoloactionl.ts
@@ -5,8 +5,8 @@ interface Coordinate {
   longitude: number
 }
 
-export const useGeolocation = () => {
-  const coords = ref<Coordinate>({ latitude: 0, longitude: 0 })
+export function useGeolocation<T> () {
+  const coords = ref<T | Coordinate>({ latitude: 0, longitude: 0 })
   const isSupported = 'navigator' in Window && 'geolocation' in navigator
 
   let watcher: number
@@ -22,6 +22,4 @@ export const useGeolocation = () => {
       navigator.geolocation.clearWatch(watcher)
     }
   })
-
-  return coords
 }
